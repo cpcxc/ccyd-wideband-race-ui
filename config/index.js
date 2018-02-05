e4a582eb97bf8c2d20e055943dcdc0a6060ec99b
dygapp <dygapp@qq.com>
2018-02-05 20:08:15 +0800

    1.0.0

--- config/index.js
+++ config/index.js
@@ -65,7 +65,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/party/',
+    assetsPublicPath: '/bandrace/',
 
     /**
      * Source Maps
